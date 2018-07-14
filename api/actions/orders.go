79fa84604df5b8dc0bf0e8ebc5bc7e30d32ae638
volodimyr <l.volodimyr@gmail.com>
2018-07-14 13:02:47 +0300

    Change database.yml and handler order

--- api/actions/orders.go
+++ api/actions/orders.go
@@ -4,7 +4,7 @@ import "github.com/gobuffalo/buffalo"
 
 // OrdersCreate default implementation.
 func OrdersCreate(c buffalo.Context) error {
-	return c.Render(200, r.JSON("orders/create.html"))
+	return c.Render(200, r.JSON("orders/create.json"))
 }
 
 // OrdersUpdate default implementation.
